d3a70411e5ff4c18f44774211b81f4c4a61474a5
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2024-09-03 22:12:12 +0530

    added

--- src/main.js
+++ src/main.js
@@ -1,107 +1,43 @@
-let shop = document.getElementById("shop");
+const shop = document.querySelector("#shop")
 
-/**
- * ! Basket to hold all the selected items
- * ? the getItem part is retrieving data from the local storage
- * ? if local storage is blank, basket becomes an empty array
- */
+let generateShop = ()=>{
+    return shop.innerHTML = (shopItemsData.map((x)=>{
+        //the map will target each object in the array and will execute the function one by one
+        let{id,img,name,desc,price}=x
 
-let basket = JSON.parse(localStorage.getItem("data")) || [];
+        return `<div id=product-id-${id} class="item">
+                <img  src="${img}" alt="img">
+                <div class="details">
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
+                    <div class="price-section">
+                        <h2>$${price}</h2>
+                        <div class="buttons">
+                            <i class="fa-solid fa-minus"></i>
+                            <div id=${id} class="quantity">0</div>
+                            <i class="fa-solid fa-plus"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+    }).join(""))
+}
+generateShop()
 
-/**
- * ! Generates the shop with product cards composed of
- * ! images, title, price, buttons, description
- */
 
-let generateShop = () => {
-  return (shop.innerHTML = shopItemsData
-    .map((x) => {
-      let { id, name, desc, img, price } = x;
-      let search = basket.find((y) => y.id === id) || [];
-      return `
-    <div id=product-id-${id} class="item">
-      <img width="220" src=${img} alt="">
-      <div class="details">
-        <h3>${name}</h3>
-        <p>${desc}</p>
-        <div class="price-quantity">
-          <h2>$ ${price} </h2>
-          <div class="buttons">
-            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-            <div id=${id} class="quantity">${
-        search.item === undefined ? 0 : search.item
-      }</div>
-            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-          </div>
-        </div>
-      </div>
-  </div>
-    `;
-    })
-    .join(""));
-};
+function sideBarToggle(){
+    const sideBarToggle = document.querySelector(".sideBarToggle")
+const closeBtn = document.querySelector(".closeBtn")
+const sidebar = document.querySelector(".sidebar")
+sideBarToggle.addEventListener("click",()=>{
+    console.log("he");
+    sidebar.classList.toggle("toShowSideBar")
 
-generateShop();
+})
+closeBtn.addEventListener("click",()=>{
+    console.log("hi");
+    sidebar.classList.remove("toShowSideBar")
 
-/**
- * ! used to increase the selected product item quantity by 1
- */
-
-let increment = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
-
-  if (search === undefined) {
-    basket.push({
-      id: selectedItem.id,
-      item: 1,
-    });
-  } else {
-    search.item += 1;
-  }
-
-  console.log(basket);
-  update(selectedItem.id);
-  localStorage.setItem("data", JSON.stringify(basket));
-};
-
-/**
- * ! used to decrease the selected product item quantity by 1
- */
-
-let decrement = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
-
-  if (search === undefined) return;
-  else if (search.item === 0) return;
-  else {
-    search.item -= 1;
-  }
-
-  update(selectedItem.id);
-  basket = basket.filter((x) => x.item !== 0);
-  console.log(basket);
-  localStorage.setItem("data", JSON.stringify(basket));
-};
-
-/**
- * ! To update the digits of picked items on each item card
- */
-
-let update = (id) => {
-  let search = basket.find((x) => x.id === id);
-  document.getElementById(id).innerHTML = search.item;
-  calculation();
-};
-
-/**
- * ! To calculate total amount of selected Items
- */
-
-let calculation = () => {
-  let cartIcon = document.getElementById("cartAmount");
-  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
-};
-
-calculation();
+})
+}
+sideBarToggle()
