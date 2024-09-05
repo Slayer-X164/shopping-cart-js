3f3ea3eb0b2bf20243b8e2204c0c13cdef548e91
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2024-09-05 22:02:55 +0530

    added local storage

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 const shop = document.querySelector("#shop")
-let basket=[]
+let basket= JSON.parse(localStorage.getItem("data")) || []
 
 let generateShop = ()=>{
     return shop.innerHTML = (shopItemsData.map((x)=>{
         //the map will target each object in the array and will execute the function one by one
         let{id,img,name,desc,price}=x
-
+        let search = basket.find((x)=>x.id===id) || []
         return `<div id=product-id-${id} class="item">
                 <img  src="${img}" alt="img">
                 <div class="details">
@@ -15,7 +15,7 @@ let generateShop = ()=>{
                         <h2>$${price}</h2>
                         <div class="buttons">
                             <i onclick='decrement(${id})' class="fa-solid fa-minus"></i>
-                            <div id=${id} class="quantity">0</div>
+                            <div id=${id} class="quantity">${search.item===undefined? 0: search.item}</div>
                             <i onclick='increment(${id})' class="fa-solid fa-plus"></i>
                         </div>
                     </div>
@@ -36,6 +36,7 @@ let increment = (id)=>{
     }else{
         search.item+=1
     }
+    localStorage.setItem("data",JSON.stringify(basket))
     update(selectedItem.id)
     //  console.log(basket);
 
@@ -49,6 +50,7 @@ let decrement = (id)=>{
     }else{
         search.item-=1
     }
+    localStorage.setItem("data",JSON.stringify(basket))
     update(selectedItem.id)
     // console.log(basket);
 }
@@ -57,8 +59,15 @@ let update = (id)=>{
     let search = basket.find((x)=>x.id===id)
     document.getElementById(id).innerHTML=search.item
     // console.log(search.item);
+    calculation()
 
 }
+
+let calculation = ()=>{
+    let cartCount=document.getElementById("cartCount")
+    cartCount.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0)
+}
+calculation()
 
 function sideBarToggle(){
     const sideBarToggle = document.querySelector(".sideBarToggle")
